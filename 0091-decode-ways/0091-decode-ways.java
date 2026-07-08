703192212bba4d33e9517714a4c08a7decf153ee
Utkarsh Tiwari <150710922+utkarsh7811@users.noreply.github.com>
2026-07-08 18:46:15 +0530

    Time: 1 ms (80.82%), Space: 43.4 MB (14.22%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numDecodings(String s) {
+        int length = s.length();
+        int[] dp = new int[length + 1];
+        dp[0] = 1;
+      
+        for (int i = 1; i <= length; i++) {
+            if (s.charAt(i - 1) != '0') {
+                dp[i] = dp[i - 1];
+            }
+          
+            if (i > 1 && s.charAt(i - 2) != '0') {
+                int twoDigitNumber = Integer.valueOf(s.substring(i - 2, i));
+                if (twoDigitNumber <= 26) {
+                    dp[i] += dp[i - 2];
+                }
+            }
+        }
+      
+        return dp[length];
+    }
+}
