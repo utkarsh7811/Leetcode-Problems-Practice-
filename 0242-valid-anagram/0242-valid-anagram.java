0815b5a60184547ed12d92191c473de144571baf
Utkarsh Tiwari <150710922+utkarsh7811@users.noreply.github.com>
2026-07-27 23:51:22 +0530

    Time: 19 ms (10.53%), Space: 46.5 MB (25.59%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        for(int i = 0; i < t.length(); i++){
+
+            char ch = t.charAt(i);
+
+            if(!map.containsKey(ch)){
+                return false;
+            }
+
+            map.put(ch, map.get(ch) - 1);
+
+            if(map.get(ch) == 0){
+                map.remove(ch);
+            }
+        }
+
+        return map.isEmpty();
+    }
+}
