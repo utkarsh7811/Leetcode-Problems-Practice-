99ac32e80743db886afa471a13b5f165688f56ac
Utkarsh Tiwari <150710922+utkarsh7811@users.noreply.github.com>
2026-08-05 01:04:09 +0530

    Time: 200 ms (71.44%), Space: 43.4 MB (96.54%) - LeetHub

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.java
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.java
@@ -3,42 +3,29 @@ class Solution {
 
     public int zigZagArrays(int n, int l, int r) {
         int m = r - l + 1;
-
         int[] dp = new int[m];
-        java.util.Arrays.fill(dp, 1);
+        Arrays.fill(dp, 1);
 
-        for (int len = 2; len <= n; len++) {
-
-            if ((len & 1) == 0) {
-                long pref = 0;
-
-                for (int i = 0; i < m; i++) {
-                    int old = dp[i];
-
-                    dp[i] = (int) pref;
-
-                    pref = (pref + old) % MOD;
+        for (int i = 2; i <= n; i++) {
+            int sum = 0;
+            if ((i & 1) == 0)
+                for (int j = 0; j < m; j++) {
+                    int t = dp[j];
+                    dp[j] = sum;
+                    sum = (sum + t) % MOD;
                 }
-            }
-            else {
-                long suff = 0;
-
-                for (int i = m - 1; i >= 0; i--) {
-                    int old = dp[i];
-
-                    dp[i] = (int) suff;
-
-                    suff = (suff + old) % MOD;
+            else
+                for (int j = m - 1; j >= 0; j--) {
+                    int t = dp[j];
+                    dp[j] = sum;
+                    sum = (sum + t) % MOD;
                 }
-            }
         }
 
-        long ans = 0;
+        int res = 0;
+        for (int j = 0; j < m; j++)
+            res = (res + dp[j]) % MOD;
 
-        for (int x : dp) {
-            ans = (ans + x) % MOD;
-        }
-
-        return (int) ((ans * 2) % MOD);
+        return (res << 1) % MOD;
     }
 }
