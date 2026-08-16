ba59223a23834bd9c2e9371066fd8761928f85dd
Utkarsh Tiwari <150710922+utkarsh7811@users.noreply.github.com>
2026-08-16 15:41:02 +0530

    Time: 2 ms (66.94%), Space: 43.3 MB (20.27%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,15 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        long sum = 0, diff = 0;
+
+        for (char c : t.toCharArray()) {
+            sum += c - 'a';
+        }
+
+        for (char c : s.toCharArray()) {
+            diff += c - 'a';
+        }
+
+        return (char) (sum - diff + 'a');
+    }
+}
